30b55e326fc489cad3863ead89bf9e947e17b010
KarimAbouelNaga <karim_abouelnaga@hotmail.com>
2020-04-15 16:39:51 +0200

    gamelobby(dashboard) + profile
    not done yet, needs better integration

---
+++ src/components/AvatarCircle/AvatarCircle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function AvatarCircle(props) {
+  return (
+    <div
+      className={["circle-crop", props.className].join(" ")}
+      style={{
+        backgroundImage: `url(${props.avatarUrl})`,
+        width: props.size,
+        height: props.size,
+      }}
+    ></div>
+  );
+}
