9484a8ae2544b632fff184312c781466d78dce8b
ianskoo <christian.skorski@outlook.com>
2020-04-17 20:44:30 +0200

    Fixed routing from login to new lobby/dashboard

--- src/components/shared/routers/GameRouter.js
+++ src/components/shared/routers/GameRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Redirect, Route } from "react-router-dom";
 import Game from "../../game/Game";
+import Dashboard from "../../Dashboard/Dashboard";
 
 const Container = styled.div`
   display: flex;
@@ -18,7 +19,7 @@ class GameRouter extends React.Component {
         <Route
           exact
           path={`${this.props.base}/dashboard`}
-          render={() => <Game />}
+          render={() => <Dashboard />}
         />
 
         <Route
