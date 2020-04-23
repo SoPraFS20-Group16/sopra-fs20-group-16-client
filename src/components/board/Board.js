124caf6f6c8fd917d597fc1c493e09528a0474af
epura <eleonora_pura@hotmail.it>
2020-04-23 14:14:39 +0200

    adjusted axial coordinates

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -10,7 +10,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             hexSize: 60,
-            hexOrigin: {x: 100, y: 70}
+            hexOrigin: {x: 400, y: 340}
 
         };
     }
@@ -18,7 +18,7 @@ class Board extends React.Component {
     componentWillMount() {
         let hexParameters = this.getHexParameters()
         this.setState({
-            canvasSize : {canvasWidth:900, canvasHeight:700},
+            canvasSize : {canvasWidth:800, canvasHeight:600},
             hexParameters : hexParameters
         })
     }
@@ -26,85 +26,102 @@ class Board extends React.Component {
 
     componentDidMount() {
         const {canvasWidth, canvasHeight} = this.state.canvasSize;
+        //assigning canvasWidth to the one chosen in setState
         this.canvasHex.width = canvasWidth;
+        //assigning canvasHeight to the one chosen in setState
         this.canvasHex.height = canvasHeight;
         //this.drawHex(this.canvasHex, { x: 50, y: 50})
+        //this.drawHex(this.canvasHex, { x: 452, y: 492})
         this.drawHexes();
     }
+
 
     drawHexes(){
         const {canvasWidth, canvasHeight} = this.state.canvasSize;
         const {hexWidth, hexHeight, vertDist, horizDist} = this.state.hexParameters;
         const hexOrigin = this.state.hexOrigin;
-        let qLeftSide =  Math.round( hexOrigin.x/hexWidth);
-        let qRightSide = Math.round(canvasWidth - hexOrigin.x)/hexWidth;
 
+        //q --> columns    r--> rows
 
-        for (let q=2; q<=4; q++){
+        for (let q=0; q<=2; q++){
+            let center = this.hexToPixel(this.Hex(q,-2));
+            this.drawHex(this.canvasHex, center);
+            this.drawHexCoordinates(this.canvasHex,center,this.Hex(q,-2));
+        }
+
+        for (let q=-1; q<=2; q++){
+            let center = this.hexToPixel(this.Hex(q,-1));
+            this.drawHex(this.canvasHex, center);
+            this.drawHexCoordinates(this.canvasHex,center,this.Hex(q,-1));
+        }
+
+        for (let q=-2; q<=2; q++){
             let center = this.hexToPixel(this.Hex(q,0));
             this.drawHex(this.canvasHex, center);
+            this.drawHexCoordinates(this.canvasHex,center,this.Hex(q,0));
         }
 
-        for (let q=1; q<=4; q++){
+        for (let q=-2; q<=1; q++){
             let center = this.hexToPixel(this.Hex(q,1));
             this.drawHex(this.canvasHex, center);
+            this.drawHexCoordinates(this.canvasHex,center,this.Hex(q,1));
         }
 
-        for (let q=0; q<=4; q++){
-            let center = this.hexToPixel(this.Hex(q,2));
+        for (let q=-2; q<=0; q++) {
+            let center = this.hexToPixel(this.Hex(q, 2));
             this.drawHex(this.canvasHex, center);
-        }
-
-        for (let q=0; q<=3; q++){
-            let center = this.hexToPixel(this.Hex(q,3));
-            this.drawHex(this.canvasHex, center);
-        }
-
-        for (let q=0; q<=2; q++) {
-            let center = this.hexToPixel(this.Hex(q, 4));
-            this.drawHex(this.canvasHex, center);
+            this.drawHexCoordinates(this.canvasHex,center,this.Hex(q,2));
         }
     }
+
+
+
 
 
     getHexesArray(){
         const hexesArray = Array(19);
 
-        for (let q=2; q<=4; q++){
+        for (let q=0; q<=2; q++){
+            let center = this.hexToPixel(this.Hex(q,-2));
+            hexesArray.push(this.createHex(center));
+        }
+
+        for (let q=-1; q<=2; q++){
+            let center = this.hexToPixel(this.Hex(q,-1));
+            hexesArray.push(this.createHex(center));
+        }
+
+        for (let q=-2; q<=2; q++){
             let center = this.hexToPixel(this.Hex(q,0));
             hexesArray.push(this.createHex(center));
         }
 
-        for (let q=1; q<=4; q++){
+        for (let q=-2; q<=1; q++){
             let center = this.hexToPixel(this.Hex(q,1));
             hexesArray.push(this.createHex(center));
         }
 
-        for (let q=0; q<=4; q++){
-            let center = this.hexToPixel(this.Hex(q,2));
-            hexesArray.push(this.createHex(center));
-        }
-
-        for (let q=0; q<=3; q++){
-            let center = this.hexToPixel(this.Hex(q,3));
-            hexesArray.push(this.createHex(center));
-        }
-
-        for (let q=0; q<=2; q++) {
-            let center = this.hexToPixel(this.Hex(q, 4));
+        for (let q=-2; q<=0; q++) {
+            let center = this.hexToPixel(this.Hex(q, 2));
             hexesArray.push(this.createHex(center));
         }
         return hexesArray;
     }
 
 
-
+    //draw one hexagon taking the canvasID and the center
     drawHex(canvasID, center){
         for(let i=0; i<=5; i++){
             let start = this.getHexCornerCoord(center,i);
 
-            let end = this.getHexCornerCoord(center, i+1);
+            console.log(start);
 
+            //get next corner by incrementing the index (i+1)
+            let end = this.getHexCornerCoord(center, i+1);
+            console.log(end);
+
+
+            //draw line between the 2 points
             this.drawLine(canvasID, { x: start.x, y: start.y }, { x: end.x, y: end.y })
         }
     }
@@ -125,14 +142,19 @@ class Board extends React.Component {
         return <Hexagon coords = {coordinates}/>;
     }
 
+    //calculate hexagon coordinates relatively to the center and the index of the corner (i)
     getHexCornerCoord(center, i){
         let angle_deg = 60*i+30;
         let angle_rad = Math.PI/180*angle_deg;
         let x = center.x + this.state.hexSize*Math.cos(angle_rad);
         let y = center.y + this.state.hexSize * Math.sin(angle_rad);
+
+        //return x and y coordinates of the corner
         return this.Point(x, y);
     }
 
+
+    //get axial coordinates
     hexToPixel(h){
         let hexOrigin = this.state.hexOrigin;
         let x = this.state.hexSize*Math.sqrt(3)*(h.q+h.r/2)+hexOrigin.x;
@@ -148,6 +170,7 @@ class Board extends React.Component {
         return {q: q, r: r}
     }
 
+    //draw line between one point (start) and another point (end)
     drawLine(canvasID, start, end){
         const ctx = canvasID.getContext("2d");
         ctx.beginPath();
