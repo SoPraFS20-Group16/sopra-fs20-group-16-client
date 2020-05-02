9bffd088d6c63f493c93f44493471cd541ea581f
KarimAbouelNaga <karim.abouelnaga@uzh.ch>
2020-05-02 15:13:46 +0200

    finished lobby

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,10 +8,7 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state  = {
-            moves: '',
-            players: [],
-            id: '',
-            gameState: false
+            gameDetails: {}
         };
         this.startGamehandler = this.startGamehandler.bind(this);
         this.interval = null;
@@ -22,48 +19,15 @@ class Home extends React.Component {
         this.props.history.push("/login");
     }
 
-
-
-    /* componentDidMount() {
+    componentDidMount() {
         this.interval = setInterval(() => {
             api.get("/games/"+ this.props.match.params.id ).then((res) => {
-                this.setState(res.data);
+                this.setState({ gameDetails: {...res.data}});
                 if(res.data && res.data.started) {
                     this.props.history.push("/game/"+this.props.match.params.id+"/dashboard");
                 }
             })
         }, 1000);
-    } */
-
-
-    componentDidMount() {
-
-            api.get("/games/"+ this.props.match.params.id ).then((res) => {
-                this.state.id = JSON.stringify(res.data.gameId);
-                this.gameState = res.data.started;
-                console.log(res.data.moves.length, "what is your plan");
-                if (res.data.moves.length === 0) {
-                    this.setState({
-                        moves:''
-
-                    });
-                    // if(res.data && res.data.started) {
-                    //     this.props.history.push("/game/"+this.props.match.params.id+"/dashboard");
-                    //}
-                }
-                else if (res.data.players.length > 1) {
-                    this.setState({
-                            moves: res.data.moves[0].moveId,
-                            players: res.data.players
-
-                        });
-                        // if(res.data && res.data.started) {
-                        //     this.props.history.push("/game/"+this.props.match.params.id+"/dashboard");
-                        // }
-                }
-                console.log(this.state, "show the state")
-            })
-        console.log(this.props.match, "coming from props")
 
     }
 
@@ -72,39 +36,17 @@ class Home extends React.Component {
     }
 
     async startGamehandler () {
-        const token  =  localStorage.getItem("token");
-        console.log(token)
-        console.log(this.state.moves, "moveid checker?")
-
 
         const requestBody = {
-            "moveId": this.state.moves
+            moveId: this.state.gameDetails.moves[0].moveId
         }
-        console.log(requestBody, "what is the requestbody?")
         await api.put("/games/" + this.props.match.params.id, requestBody).then(
             this.props.history.push("/game/"+ this.props.match.params.id+"/dashboard")
         )
 
-
-
-       // this.props.history.push("/game/"+ this.props.match.params.id+"/dashboard")
-   // fetch("http://localhost:3000/games/"+ this.props.match.params.id, {
-   //     method: "PATCH",
-   //     headers:{
-   //         "Content-type": "application/json",
-   //         Authorization: token
-   //     },
-   //     body: JSON.stringify({
-   //         moveId: this.state.moves
-   //     })
-   // } )
-   //     .then(res => res.json())
-   //     .then(res => console.log(res, "coming from put"))
-   //     .catch(err => console.log(err, "sorry cannot do a put request"))
 }
 
     render() {
-
     return <div className="body1"><div className="center">
         <div className="left">
             <h1 className="heading">
@@ -131,12 +73,12 @@ class Home extends React.Component {
             </div>
             <div className="down">
                 <div className="box3">
-                    {this.state && this.state.players && this.state.players.map((player, index) => <p key={index}>{player.username}</p>)}
+                    {this.state.gameDetails.players && this.state.gameDetails.players.map((player, index) => <p key={index}>{player.username}</p>)}
                 </div>
-                <div className="box4">
+                {this.state.gameDetails.moves && this.state.gameDetails.moves.length > 0 && <div className="box4">
                     <p>Let's start this game </p>
                     <img onClick={this.startGamehandler} src={require('./send.svg')} height={'20px'} width="20px"/>
-                </div>
+                </div>}
                 <div className="buttons">
                         <p className="button1">
                             <a href="#" onClick={() => this.logout()}>Logout</a></p>
