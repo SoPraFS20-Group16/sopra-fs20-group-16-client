63caebcb828b4e275e18e7e3ad5a59cce4688769
KarimAbouelNaga <karim_abouelnaga@hotmail.com>
2020-05-08 19:35:38 +0200

    including map in lobby

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,11 @@ import {Link, withRouter} from "react-router-dom";
 import { api } from "../../helpers/api";
 import './style.css';
 import styled from "styled-components";
+import MapGL, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import pin from './pin.png';
+
+
 
 const GoldButt = styled.button`
     padding: 17px;
@@ -23,16 +28,56 @@ const GreenButt = styled.button`
     margin: 20px;
 `;
 
+const TOKEN =
+    "pk.eyJ1Ijoia2FyaW1hYm91ZWxuYWdhMiIsImEiOiJjazl5MHkwMGEwZ3UzM2dsaXpmc21kMW54In0.IUzrOcWiWDGBpcVKsFCX-g";
+
+const latlong = [
+    { name: "Tokyo", latitude: 35.652832, longitude: 139.839478 }
+];
+
+const Markers = () => {
+    // const {data} = this.props;
+    return latlong.map(city => (
+        <Marker
+            key={city.name}
+            offsetLeft={-19}
+            offsetTop={-37}
+            longitude={city.longitude}
+            latitude={city.latitude}
+        >
+            <img src={pin} height="42" width="42" />
+        </Marker>
+    ));
+};
+
+
+
 class Home extends React.Component {
 
     constructor(props) {
         super(props);
         this.state  = {
-            gameDetails: {}
+            gameDetails: {},
+            viewport: {
+                width: "100%",
+                height: 450,
+                latitude: 20,
+                longitude: 0,
+                zoom: 1
+            }
         };
         this.startGamehandler = this.startGamehandler.bind(this);
+        this._onViewportChange = this._onViewportChange.bind(this);
         this.interval = null;
     }
+
+    _onViewportChange = viewport =>
+        this.setState({
+            viewport: {
+                ...viewport,
+                transitionDuration: 0
+            }
+        });
 
     async logout() {
         await api.put("/logout", null, {
@@ -83,25 +128,24 @@ class Home extends React.Component {
 
         </div>
 
-        <div className="center">
+        <div className="mapContainer">
+            <MapGL
+                {...this.state.viewport}
+                mapboxApiAccessToken={TOKEN}
+                mapStyle="mapbox://styles/karimabouelnaga2/ck9y14ojn1yfd1iphribr8z9e"
+                onViewportChange={this._onViewportChange}
+            >
+                <Markers />
+            </MapGL>
+        </div>
 
-        <div className="right">
-            <div className="down">
-
-                <div className="containerGameInfos">
+                <div className="containerPlayerInfos">
                     {this.state.gameDetails.players && this.state.gameDetails.players.map((player, index) => <p key={index}>{player.username}</p>)}
                 </div>
 
-                {this.state.gameDetails.moves && this.state.gameDetails.moves.length > 0 && <div className={"start_button"}>
+                {this.state.gameDetails.moves && this.state.gameDetails.moves.length > 0 && <div className="start_button">
                     <GreenButt onClick={this.startGamehandler}>Start the Game</GreenButt>
                 </div>}
-
-
-
-            </div>
-        </div>
-
-    </div>
     </div>
   }
 }
