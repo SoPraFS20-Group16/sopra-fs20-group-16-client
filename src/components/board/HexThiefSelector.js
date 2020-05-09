106ed2c170d2371f537f4fa080c9e4ae3450bab9
ianskoo <christian.skorski@outlook.com>
2020-05-09 18:43:41 +0200

    Created hex overlay button for moving the thief

---
+++ src/components/board/HexThiefSelector.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Hexagon from "react-svg-hexagon"
+import {api} from "../../helpers/api";
+
+export default function HexThiefSelector(props) {
+
+  const requestBody = JSON.stringify({
+    moveId: props.moveId
+  });
+
+  async function handler(){
+    await api.put("/games/" + props.gameId, requestBody);
+  }
+
+  return (
+    <button style={{
+      position: "absolute",
+      left: props.x + 43.30127,
+      top: props.y + props.side,
+      transform: "translate(-50%, -50%)",
+
+      bottom: '33%',
+      height: props.side*1.5,
+      width: props.side*1.5,
+      backgroundColor: 'rgba(50,150,50, 0.5)',
+      borderRadius: 1000,
+    }}
+    onClick={handler}
+    >
+    </button>
+  )
+}
+
