565b131e2b07529b8941f219ea3af39fa34274b0
ianskoo <christian.skorski@outlook.com>
2020-05-13 19:47:50 +0200

    Trying to implement better randomised dice, problems with react refreshing them every 1s for no reason

--- src/components/game/Dice.js
+++ src/components/game/Dice.js
@@ -8,17 +8,55 @@ import d5 from '../../views/graphics/dice/dice_5.png'
 import d6 from '../../views/graphics/dice/dice_6.png'
 
 
-export default function dice(props) {
+export default class Dice extends React.Component {
 
-  const dieList = [d1, d2, d3, d4, d5, d6];
-  let dice1 = Math.floor(props.result / 2);
-  let dice2 = props.result - dice1;
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      dieList: [d1, d2, d3, d4, d5, d6],
 
+    }
+    this.state.result = props.result;
+  }
 
-  return(
-    <div className={'diceBox'}>
-      <img src={dieList[dice1 - 1]} alt={''} style={{border:'1px solid black', borderRadius:'5px'}} />
-      <img src={dieList[dice2 - 1]} alt={''} style={{border:'1px solid black', borderRadius:'5px'}} />
+  genNums() {
+    const result = this.state.result;
+    let dice1 = Math.floor(result / 2);
+    let dice2 = result - dice1;
+
+    // Create a "drifter" to make dice numbers more interesting and randomized
+    let drifter;
+    if(result >= 6 && result <= 8){drifter = 3.33}
+    else if((result >= 4 && result <= 5) || (result >= 9 && result <= 10)){
+      drifter = 2.22;
+    }
+    else {drifter = 1.11}
+
+    let sign = 1;
+    if(Math.random() <= 0.4){sign = -1;}
+
+    let randomizer = sign * Math.floor(Math.random() * drifter);
+
+    // Apply randomization to created dice values
+    dice1 += randomizer;
+    dice2 -= randomizer;
+
+    console.log('dice: ' + dice1 + ' ' +  dice2)
+
+    return {dice1: dice1, dice2: dice2}
+  }
+
+  render(){
+  return (
+    <div>
+      {this.state.result !== 0 &&
+        <div className={'diceBox'}>
+          <img src={this.state.dieList[this.genNums().dice1 - 1]} alt={''} style={{border: '1px solid black', borderRadius: '5px'}}/>
+          <img src={this.state.dieList[this.genNums().dice2 - 1]} alt={''} style={{border: '1px solid black', borderRadius: '5px'}}/>
+        </div>
+      }
     </div>
-)
+  );
+  }
 }
