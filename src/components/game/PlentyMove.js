52883362f07825a6f09899bea4ccd0a85b71a5d7
r-s-3 <45521666+r-s-3@users.noreply.github.com>
2020-05-20 13:22:45 +0200

    (FS20G16-32) implemented Plenty move functionality

--- src/components/game/PlentyMove.js
+++ src/components/game/PlentyMove.js
@@ -6,122 +6,54 @@ import Brick from '../../views/graphics/brick.png'
 import Wool from '../../views/graphics/wool.png'
 import {api} from "../../helpers/api";
 
-export default class PlentyMove extends React.Component{
+export default function PlentyMove(props){
+    const imgSize = 35;
 
-  constructor(props) {
-    super(props);
-    this.state={
-      firstRes: "",
-      secondRes:"",
-      count:0,
-      imgSize:35
-    };
-    this.resetState = this.resetState.bind(this);
-    this.incrementCount = this.incrementCount.bind(this);
-    this.setResources = this.setResources.bind(this);
-  }
+    function selectPlentyType(type){
 
-  checkIfBothEmpty(){
-    return (this.state.firstRes === "" && this.state.secondRes === "");
-  }
+      let moveId;
+      const moves = props.moves;
 
-  resetState(){
-    this.setState({firstRes:"", secondRes : "", count: 0});
-  }
+      moves.map((move) => {
+        if(move.plentyType === type){
+          moveId = move.moveId;
+        }
+      });
 
-  incrementCount(){
-    this.setState({count: this.state.count + 1})
-  }
-
-
-  setResources(resource){
-    let moveId;
-    const moves = this.props.moves;
-
-    if (this.state.count === 0){
-      if(this.state.firstRes === ""){
-        this.setState({firstRes: resource})
-      }
+      return moveId;
     }
 
-    if (this.state.count ===1){
-      if(this.state.firstRes !== "" && this.state.secondRes ==="" && this.state.count ===1){
-        this.setState({secondRes: resource})
-      }
-    }
-
-    if(this.state.count === 2){
-      if(this.state.firstRes !== "" && this.state.secondRes !== ""){
-        moves.map((move) =>{
-          if(move.plentyType1 === this.state.firstRes && move.plentyType2 === this.state.secondRes){
-            moveId = move.moveId;
-          }
-        });
-        return async () => await api.put("/games/" + this.props.gameId, JSON.stringify({moveId: moveId})) && this.resetState();
-      }
-    }
-    this.incrementCount();
-  }
-
-
-
-
-  render(){
-    const {imgSize} = this.state;
     return(
-      <div className={'plentyAndMonopoly'} style={{justifyContent: "center"}}>
-        <h5> <b>Select 2 resources! </b></h5>
+        <div className={'plentyAndMonopoly'}>
+          <h5> <b>Pick a Type!</b> </h5>
 
-        <div>
+          <div>
             <button
-              style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
-              onClick={this.setResources("LUMBER")}
-              disabled = {this.checkIfBothEmpty() === true}
+                style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
+                onClick={async () => await api.put("/games/" + props.gameId, JSON.stringify({moveId: selectPlentyType("MINER")}))}
             >
-              <img style={{height: imgSize, textAlign: 'center'}} src = {Lumber} alt = ""/>
+              <img style={{height: imgSize, textAlign: 'center'}} src = {Brick} alt = ""/>
             </button>
 
             <button
-              style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
-              onClick={this.setResources("GRAIN")}
-              disabled={this.checkIfBothEmpty() === true}
+                style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
+                onClick={async () => await api.put("/games/" + props.gameId, JSON.stringify({moveId: selectPlentyType("FARMER")}))}
             >
-              <img style={{height: imgSize, textAlign:'center'}} src = {Grain} alt = ""/>
+              <img style={{height: imgSize}} src = {Grain} alt = ""/>
             </button>
+          </div>
+
+          <div>
+            <button
+                style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
+                onClick={async () => await api.put("/games/" + props.gameId, JSON.stringify({moveId: selectPlentyType("LUMBERJACK")}))}
+            >
+              <img style={{height: imgSize}} src = {Lumber} alt = ""/>
+            </button>
+
+          </div>
+
         </div>
-
-        <div>
-          <button
-            style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
-            onClick={this.setResources("BRICK")}
-            disabled={this.checkIfBothEmpty() === true}
-          >
-            <img style={{height: imgSize, textAlign:'center'}} src = {Brick} alt = ""/>
-          </button>
-
-
-          <button
-            style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
-            onClick={this.setResources("WOOL")}
-            disabled={this.checkIfBothEmpty() === true}
-          >
-            <img style={{height: imgSize, textAlign:'center'}} src = {Wool} alt = ""/>
-          </button>
-        </div>
-
-        <div>
-          <button
-            style={{justifyContent: "center", backgroundColor: "transparent", border: "1px solid transparent"}}
-            onClick={this.setResources("ORE")}
-            disabled={this.checkIfBothEmpty() === true}
-          >
-            <img style={{height: imgSize, textAlign:'center'}} src = {Ore} alt = ""/>
-          </button>
-        </div>
-      </div>
 
     )
-  }
-
-
 }
